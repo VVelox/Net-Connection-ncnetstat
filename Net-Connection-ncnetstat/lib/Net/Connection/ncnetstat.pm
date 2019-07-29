4b611b60b854b3b22b80f826e6996030ba743666
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2019-07-29 01:05:15 -0500

    add initial files

---
+++ Net-Connection-ncnetstat/lib/Net/Connection/ncnetstat.pm
@@ -0,0 +1,109 @@
+package Net::Connection::ncnetstat;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+Net::Connection::ncnetstat - The great new Net::Connection::ncnetstat!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Net::Connection::ncnetstat;
+
+    my $foo = Net::Connection::ncnetstat->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Zane C. Bowers-Hadley, C<< <vvelox at vvelox.net> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-net-connection-ncnetstat at rt.cpan.org>, or through
+the web interface at L<https://rt.cpan.org/NoAuth/ReportBug.html?Queue=Net-Connection-ncnetstat>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Net::Connection::ncnetstat
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<https://rt.cpan.org/NoAuth/Bugs.html?Dist=Net-Connection-ncnetstat>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Net-Connection-ncnetstat>
+
+=item * CPAN Ratings
+
+L<https://cpanratings.perl.org/d/Net-Connection-ncnetstat>
+
+=item * Search CPAN
+
+L<https://metacpan.org/release/Net-Connection-ncnetstat>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+This software is Copyright (c) 2019 by Zane C. Bowers-Hadley.
+
+This is free software, licensed under:
+
+  The Artistic License 2.0 (GPL Compatible)
+
+
+=cut
+
+1; # End of Net::Connection::ncnetstat
