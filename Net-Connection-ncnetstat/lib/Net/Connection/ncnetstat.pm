6d64af3c1be9ddcb5bb42534d118cc742d6c2083
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2019-08-06 05:33:58 -0500

    ready to release 0.1.0

--- Net-Connection-ncnetstat/lib/Net/Connection/ncnetstat.pm
+++ Net-Connection-ncnetstat/lib/Net/Connection/ncnetstat.pm
@@ -17,11 +17,11 @@ Net::Connection::ncnetstat - The backend for ncnetstat, the colorized and enhanc
 
 =head1 VERSION
 
-Version 0.0.1
+Version 0.1.0
 
 =cut
 
-our $VERSION = '0.0.1';
+our $VERSION = '0.1.0';
 
 
 =head1 SYNOPSIS
